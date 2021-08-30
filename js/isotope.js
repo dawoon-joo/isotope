0e3ee63bf156321c734e8c769ad9b350bfbc0a47
dawoon-joo <wnekdns5015@gamil.con>
2021-08-30 18:52:49 +0900

    isotope 포폴완성

---
+++ js/isotope.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+    var $grid;
+
+    $grid = $(".wrap").isotope({ //모션을 적용할 박스의 부모 선택자명
+        itemSelector: ".wrap article", //모션을 적용할 박스 선택자명
+        columnWidth: ".wrap article", // 너비값을 구할 박스 선택자명
+        transitionDuration: "0.8s", // 모션 이동 속도
+        percentPosition: true
+    });
+
+    $(".filter li a").on("click", function (e) {
+        e.preventDefault();
+
+        var sort = $(this).attr("href");
+
+        $grid.isotope({
+            filter: sort
+        });
+
+        $(".filter li a").removeClass("on");
+        $(this).addClass("on");
+    });
+
+    var option = {
+		itemSelector: '.wrap article',
+	};
+    var $container = $('.wrap').imagesLoaded( function() {
+		$container.isotope(option);
+	});
+});
